13ee6cea8e23e78d952088dcbc55e1a293bb3d37
Anthony <anthonylsa12@gmail.com>
2024-03-02 12:27:03 -0800

    imported functions for components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@
 
 // Import components
 import './App.css';
+import Searchbar from './searchbar.js'
+import Directory from './directory.js'
+import Gallery from './gallery.js'
+import {postings} from './postings.js'
+import Sidebar from './sidebar.js'
 
-function App(){
+
+function App() {
   return (
     <div>
-      <h1>craigslist</h1>
+      <h1>Craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+      <div className="App">
+        <Searchbar />
+        <Directory />
+        <Sidebar />
+        <Gallery postings={postings} />
       </div>
+    </div>
     </div>
   );
 }
